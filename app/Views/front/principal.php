cc4f15b177aad1aa1345a0100acba080b39bd75f
Jeremias Gonzalez <jeremiasgonzalez7464@gmail.com>
2025-05-31 18:05:38 -0300

    add carrusel

--- app/Views/front/principal.php
+++ app/Views/front/principal.php
@@ -1,13 +1,34 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="assets/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
-    <script src="assets/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
-  </body>
-</html>
+<header class="header">
+    <div class="header-content">
+        <img class="img-logo" src="<?php echo base_url('assets/img/logoHeader-green.png'); ?>" alt="Logo">
+        <h1>Bienvenido a nuestra página principal</h1>
+    </div>
+</header>
+<div id="carrusel_presentacion" class="carousel slide">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="assets/img/carrusel_reloj1.jpg" class="carrusel-img" alt="Imagen 1">
+        </div>
+        <div class="carousel-item">
+            <img src="assets/img/carrusel_reloj2.jpg" class="carrusel-img" alt="Imagen 2">
+        </div>
+        <div class="carousel-item">
+            <img src="assets/img/carrusel_reloj3.jpg" class="carrusel-img" alt="Imagen 3">
+        </div>
+    </div>
+
+    <button class="carousel-control-prev" type="button" data-bs-target="#carrusel_presentacion" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Anterior</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carrusel_presentacion" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Siguiente</span>
+    </button>
+
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carrusel_presentacion" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carrusel_presentacion" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carrusel_presentacion" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+</div>
